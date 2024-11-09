731a7f7e0590f6504d1a6ecdeeab636698106955
AFDanao <danao.adrian@ue.edu.ph>
2024-11-09 13:58:14 +0800

    Testing Other Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Gussi" />
+      <Status status="loading" />
+      <Heading>Placeholder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name="Gussi" isLoggedIn={false} />
     </div>
   );
 }
