2a9635ce3d9795534676f342577479a9e1c5efda
AFDanao <danao.adrian@ue.edu.ph>
2024-11-10 19:41:00 +0800

    learning about style props and tips & tricks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import "./App.css";
-import { Greet } from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Status } from "./components/Status";
 
 function App() {
-  return (
-    <div className="App">
-      <Status status="loading" />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name="Gussi" isLoggedIn={false} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
